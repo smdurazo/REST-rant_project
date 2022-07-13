d1c9c67059ea9c72443a6e9569fceea53d8dd3f9
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-13 12:55:19 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'Din Tai Fung',
@@ -16,9 +20,5 @@ router.get('/', (req, res) => {
       }]      
     res.render('places/index', { places })
 })
-
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })  
 
 module.exports = router;
